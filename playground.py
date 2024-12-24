d98350271e49599af29c05dec133d764fa98d381
luojingwei <jonnylaw89@163.com>
2024-12-24 15:25:20 +0800

    feat: 更新playground

--- playground.py
+++ playground.py
@@ -1,12 +1,24 @@
-import numpy as np # type: ignore
-import random
+# import torch
+# import GPUtil
 
-myList = []
-for i in range(10):
-    sublist=[i]
-    myList.extend(sublist)
+# def query_gpu_status_with_gputil():
+#     gpus = GPUtil.getGPUs()
+#     for gpu in gpus:
+#         print(f"GPU ID: {gpu.id}")
+#         print(f"GPU Name: {gpu.name}")
+#         print(f"Load: {gpu.load*100}%")
+#         print(f"Free Memory: {gpu.memoryFree}MB")
+#         print(f"Used Memory: {gpu.memoryUsed}MB")
+#         print(f"Total Memory: {gpu.memoryTotal}MB")
+#         print(f"Temperature: {gpu.temperature} °C")
+#         print('------------------------')
 
+# # 调用函数
+# query_gpu_status_with_gputil()
 
-randList = random.sample(myList, 5)
-print(list(range(4)))
+# print(f'当前已分配显存: {torch.cuda.memory_allocated()} bytes')
+# print(f'当前保留显存: {torch.cuda.memory_reserved()} bytes')
+# print(f'最大已分配显存: {torch.cuda.max_memory_allocated()} bytes')
+# print(f'最大保留显存: {torch.cuda.max_memory_reserved()} bytes')
+
 
